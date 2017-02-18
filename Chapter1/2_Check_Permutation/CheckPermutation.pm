b5370db0bd53fc67344f4186351bc4ab805cbcc0
André Pontes <andrepontes@gmail.com>
2017-02-18 10:18:35 -0200

    1.2 - Refactoring

--- Chapter1/2_Check_Permutation/CheckPermutation.pm
+++ Chapter1/2_Check_Permutation/CheckPermutation.pm
@@ -9,8 +9,18 @@ use Exporter qw(import);
 
 our @EXPORT_OK = qw(check_permutation);
 
-# Runtime Complexity:
-# Space Complexity:
+sub sum_count_array {
+    my $count  = shift;
+    my $string = shift;
+    my $qty    = shift;
+
+    foreach my $i ( 0 .. length($string) ) {
+        $count->[ ord( substr( $string, $i, 1 ) ) ] += $qty;
+    }
+}
+
+# Runtime Complexity: O(n) - n is length of string.
+# Space Complexity: O(1).
 sub check_permutation {
     my $a = shift;
     my $b = shift;
@@ -18,13 +28,9 @@ sub check_permutation {
     return 0 if length($a) != length($b);
 
     my @count = (0) x 255;
-    foreach my $i ( 0 .. length($a) ) {
-        $count[ ord( substr( $a, $i, 1 ) ) ]++;
-    }
 
-    foreach my $i ( 0 .. length($b) ) {
-        $count[ ord( substr( $b, $i, 1 ) ) ]--;
-    }
+    sum_count_array( \@count, $a, 1 );
+    sum_count_array( \@count, $b, -1 );
 
     foreach my $x (@count) {
         return 0 if $x != 0;
