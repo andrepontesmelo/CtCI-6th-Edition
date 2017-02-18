2508c8c0e9de319357d5e8ad42385b42abcca670
André Pontes <andrepontes@gmail.com>
2017-02-18 08:05:48 -0200

    1.1 - Adding test case

--- Chapter1/1_Is_Unique/Test.pl
+++ Chapter1/1_Is_Unique/Test.pl
@@ -5,10 +5,12 @@ use warnings;
 
 use IsUnique qw(is_unique);
 
-use Test::More tests => 4;
+use Test::More tests => 5;
 
 ok( is_unique(''),    'empty string' );
 ok( is_unique('abc'), 'small non repeated string' );
 ok( !is_unique('aa'), 'minimal not unique string' );
-ok( is_unique( join '', ( 'a' .. 'z', 'A' .. 'Z', '0' .. '9' ) ),
-    'large unique string' );
+
+my $large_unique_string = join '', ( 'a' .. 'z', 'A' .. 'Z', '0' .. '9' );
+ok( is_unique($large_unique_string),          'large unique string' );
+ok( !is_unique( $large_unique_string . 'X' ), 'large not unique string' );
