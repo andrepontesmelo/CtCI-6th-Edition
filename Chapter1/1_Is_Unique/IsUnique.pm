0f35a8a202de059bcc3e56e998230c90fe52f2f9
André Pontes <andrepontes@gmail.com>
2017-02-18 08:15:28 -0200

    1.1 - Constant space solution using array of bits

--- Chapter1/1_Is_Unique/IsUnique.pm
+++ Chapter1/1_Is_Unique/IsUnique.pm
@@ -9,16 +9,18 @@ use Exporter qw(import);
 
 our @EXPORT_OK = qw(is_unique);
 
-# Runtime Complexity: O(1)
-# Space Complexity: O(n) - n is number of characters of the input string
+# Runtime Complexity: O(1) for string length > 255, O(n) otherwise.
+# Space Complexity: O(1) - Additional array of bits with fixed size.
 sub is_unique {
     my $input = shift;
 
-    my %hash = ();
-    my @chars = split '', $input;
-    foreach my $char (@chars) {
-        return 0 if exists $hash{$char};
-        $hash{$char} = 1;
+    return 0 if ( length($input) > 255 );
+
+    my @bitArray = ( '' x 255 );
+    foreach my $idx ( 0 .. length($input) ) {
+        my $ord = ord( substr( $input, $idx, 1 ) );
+        return 0 if $bitArray[$ord];
+        $bitArray[$ord] = 1;
     }
 
     return 1;
